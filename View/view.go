df8e72b4e0bae7cad50bd92da2f1a590ad216103
Syakila Kusumanda <syakilakr@gmail.com>
2024-07-24 13:31:26 +0700

    Update view.go

--- View/view.go
+++ View/view.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-func InsertMember() {
+func InsertItem() {
 	var id int
 	var nama, alamat string
 	var point float32
-	fmt.Print("masukkan id member : ")
+	fmt.Print("masukkan id item : ")
 	fmt.Scan(&id)
-	fmt.Print("masukkan nama member : ")
+	fmt.Print("masukkan nama item : ")
 	fmt.Scan(&nama)
-	fmt.Print("masukkan alamat member : ")
+	fmt.Print("masukkan alamat item : ")
 	fmt.Scan(&alamat)
-	fmt.Print("masukkan point member : ")
+	fmt.Print("masukkan point item : ")
 	fmt.Scan(&point)
-	cek := Controller.InsertMember(id, nama, alamat, point)
+	cek := Controller.InsertItem(id, nama, alamat, point)
 	if cek == true {
 		fmt.Println("data berhasil di input")
 	} else {
@@ -25,14 +25,14 @@ func InsertMember() {
 	}
 }
 
-func ReadAllMember() {
-	members := Controller.ReadAllMember()
-	if members != nil {
-		for members.Next != nil {
-			fmt.Println("Nama Member : ", members.Next.Data.Nama)
-			fmt.Println("Id Member : ", members.Next.Data.Id)
-			fmt.Println("Alamat Member : ", members.Next.Data.Alamat)
-			members = members.Next
+func ReadAllItem() {
+	items := Controller.ReadAllItem()
+	if items != nil {
+		for items.Next != nil {
+			fmt.Println("Nama Item : ", items.Next.Data.Nama)
+			fmt.Println("Id Item : ", items.Next.Data.Id)
+			fmt.Println("Alamat Item : ", items.Next.Data.Alamat)
+			items = items.Next
 		}
 	}
 }
