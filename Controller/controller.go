f5d7c8153138b4a3d8dede0016fe237d7348e96f
Syakila Kusumanda <syakilakr@gmail.com>
2024-07-24 13:23:39 +0700

    Update controller.go

--- Controller/controller.go
+++ Controller/controller.go
@@ -5,15 +5,15 @@ import (
 	"ProjectMasterItem/Node"
 )
 
-func InsertMember(id int, nama string, alamat string, point float32) bool {
+func InsertItem(id int, nama string, alamat string, point float32) bool {
 	if id > 0 && nama != "" {
-		Model.InsertMember(id, nama, alamat, point)
+		Model.InsertItem(id, nama, alamat, point)
 		return true
 	} else {
 		return false
 	}
 }
 
-func ReadAllMember() *Node.TableMember {
-	return Model.ReadAllMember()
+func ReadAllItem() *Node.TableItem {
+	return Model.ReadAllItem()
 }
